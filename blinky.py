bee294cf67a1edbf675f314c7e6fc760533b20bc
segin <98380527+SEGIN-zz@users.noreply.github.com>
2022-04-17 23:37:36 +0530

    Update blinky.py

---
+++ blinky.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+
+while True:
+    GPIO.output(11,True)
+    time.sleep(1)
+    GPIO.output(11,False)
+    time.sleep(1)
+    
